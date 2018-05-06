0f0d26ab3c62e9e0a4ddcc3a26b734497db51357
Kamilczak020 <kamilczak020@gmail.com>
2018-05-06 19:51:44 +0200

    Cleaned up RandomHanlder

--- src/handler/RandomHandler.ts
+++ src/handler/RandomHandler.ts
@@ -1,15 +1,9 @@
-import * as mathjs from 'mathjs';
+import { max, min, random, randomInt } from 'mathjs';
 import { Logger } from 'noicejs/logger/Logger';
 import { Bot } from 'src/Bot';
 import { Command } from 'src/Command';
 import { Handler, HandlerOptions } from 'src/handler/Handler';
 import { Message } from 'src/Message';
-
-enum NumberSet {
-  Natural,
-  Integer,
-  Real,
-}
 
 export interface RandomHandlerConfig {
   field: string;
@@ -46,20 +40,18 @@ export class RandomHandler implements Handler {
     this.logger.debug({ args }, 'computing random..');
     let result = 0 as number | string;
     switch (args.length) {
-      case 0: { 
-        result = this.randomDefault();
+      case 0: {
+        result = this.getRandomDefault();
         break;
       }
       case 1: {
-        const set = this.inferNumberSet(Number(args[0]));
-        result = this.randomFromNumberSet(set, Number(args[0]));
+        const precision = this.getPrecision(Number(args[0]));
+        result = this.getRandomValue(precision, Number(args[0]));
         break;
       }
       case 2: {
-        const set1 = this.inferNumberSet(Number(args[0]));
-        const set2 = this.inferNumberSet(Number(args[1]));
-        const widestSet = this.getWidestSet(set1, set2);
-        result = this.randomFromNumberSet(widestSet, Number(args[0]), Number(args[1]));
+        const precision = this.getPrecision(Number(args[0]), Number([args[1]]));
+        result = this.getRandomValue(precision, Number(args[0]), Number(args[1]));
         break;
       }
     }
@@ -75,58 +67,36 @@ export class RandomHandler implements Handler {
     return true;
   }
 
-  private randomDefault(): number {
-    return mathjs.randomInt(1, 6);
+  private getRandomDefault(): number {
+    return randomInt(1, 6);
   }
 
-  private randomFromNumberSet(set: NumberSet, val1: number, val2?: number): number | string {
-    if (isNaN(val1) || (val2 !== undefined && isNaN(val2))) {
-      throw new Error('Given values do not belong to any known number set');
+  private getRandomValue(precision: number, val1: number, val2?: number): number | string {
+    if (isNaN(val1)) {
+      throw new Error(`Provided value: ${val1} is not a number!`);
+    }
+    if (!!val2 && isNaN(val2)) {
+      throw new Error(`Provided value: ${val2} is not a number!`);
     }
 
-    if (val1 === val2) {
-      return val1;
+    const minimum = val2 === undefined ? min(val1, 0) : min(val1, val2);
+    const maximum = val2 === undefined ? max(val1, 0) : max(val1, val2);
+
+    if (precision === 0) {
+      return randomInt(minimum, maximum);
     }
 
-    const minimum = !val2 ? mathjs.min(0, val1) : mathjs.min(val1, val2);
-    const maximum = !val2 ? mathjs.max(0, val1) : mathjs.max(val1, val2);
-
-    switch (set) {
-      case NumberSet.Natural:
-        return mathjs.randomInt(0, maximum);
-      case NumberSet.Integer:
-        return mathjs.randomInt(minimum, maximum);
-      case NumberSet.Real: 
-        const precision = this.getPrecision(minimum, maximum);
-        return this.round(mathjs.random(minimum, maximum), precision);
-    }
-  }
-
-  private inferNumberSet(value: number): NumberSet {
-    if (isNaN(value)) {
-      throw new Error('Given value is not a number');
-    }
-
-    if (value >= 0) {
-      return value % 1 === 0 ? NumberSet.Natural : NumberSet.Real;
-    }
-    return value % 1 === 0 ? NumberSet.Integer : NumberSet.Real;
-  }
-
-  private getWidestSet(set1: NumberSet, set2: NumberSet) {
-    return set1.toString() > set2.toString() ? set1 : set2;
+    // Otherwise we are dealing with at least one float
+    return this.round(random(minimum, maximum), precision);
   }
 
   private getPrecision(...values: number[]) {
-    let precision = 0;
-    values.forEach(value => {
+    const precision = values.map(value => {
       const parts = value.toString().split('.');
-      if (parts[1] !== undefined) {
-        const valuePrecision = parts[1].length;
-        if (valuePrecision > precision) {
-          precision = valuePrecision;
-        }
-      }
+      return parts[1] !== undefined ? parts[1].length : 0;
+    })
+    .reduce((previous, current) => {
+      return current > previous ? current : previous;
     });
 
     return precision;
