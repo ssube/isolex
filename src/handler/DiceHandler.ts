b5b303f641306df953daf97fb23787e60c706a86
Kamilczak020 <kamilczak020@gmail.com>
2018-05-07 00:30:04 +0200

    Implemented dice handler

--- src/handler/DiceHandler.ts
+++ src/handler/DiceHandler.ts
@@ -3,54 +3,41 @@ import { Logger } from 'noicejs/logger/Logger';
 import { Bot } from 'src/Bot';
 import { Command } from 'src/Command';
 import { Handler, HandlerOptions } from 'src/handler/Handler';
+import { BaseHandler } from 'src/handler/BaseHandler';
 import { Message } from 'src/Message';
 
 export interface DiceHandlerConfig {
-  field: string;
   name: string;
 }
 
-export interface DiceHandlerOptions extends HandlerOptions<DiceHandlerConfig> {
-  // snoop
-}
+export type DiceHandlerOptions = HandlerOptions<DiceHandlerConfig>;
 
-export class DiceHandler implements Handler {
-  protected bot: Bot;
-  protected config: DiceHandlerConfig;
-  protected logger: Logger;
+export class DiceHandler extends BaseHandler<DiceHandlerConfig> implements Handler {
+  protected name: string;
   
   constructor(options: DiceHandlerOptions) {
-    this.bot = options.bot;
-    this.config = options.config;
-    this.logger = options.logger.child({
-        class: DiceHandler.name
-    });
+    super(options);
+
+    this.name = options.config.name;
   }
 
-  public async handle(cmd: Command): Promise<boolean> {
-    if (cmd.name !== this.config.name) {
-      return false;
-    }
-  
+  public async check(cmd: Command): Promise<boolean> {
+    return cmd.name === this.name;
+  }
+
+  public async handle(cmd: Command): Promise<void> {
     const args = cmd.data.get('args');
     if (!args || !args.length) {
       throw new Error('no arguments were provided!');
     }
 
-    const dicePattern = /(\d+)(?:d|D)(\d+)/;
-    const match = dicePattern.exec(args[0]);
-    if (match === null) {
-      throw new Error('Incorrect argument pattern');
-    }
-
-    this.logger.debug({ match }, 'matched dice pattern')
     let results = [] as number[];
-    for (let i = 0; i < Number(match[1]); i++) {
-      const rollResult = mathjs.randomInt(1, Number(match[2]));
+    for (let i = 0; i < Number(args[1]); i++) {
+      const rollResult = mathjs.randomInt(1, Number(args[2]));
       results.push(rollResult);
     }
 
-    this.logger.debug({ match }, 'handling dice results')
+    this.logger.debug({ args }, 'handling dice results')
     const sum = results.reduce((a, b) => { return a + b });
     const msg = Message.create({
       body: `The results of your rolls were: ${results}. The sum is ${sum}.`,
@@ -58,7 +45,6 @@ export class DiceHandler implements Handler {
       reactions: []
     });
 
-    await this.bot.send(msg);
-    return true;
+    await this.bot.send(msg);    
   }
 }
