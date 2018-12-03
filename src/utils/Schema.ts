5f7a81621b82e4cb9af804bf37dbbf9fc1378c65
Sean Sube <seansube@gmail.com>
2018-12-02 22:41:56 -0600

    add ajv dep, schema class, and some parser schemas

---
+++ src/utils/Schema.ts
@@ -0,0 +1,35 @@
+import * as Ajv from 'ajv';
+import { filterNil } from '.';
+
+export interface SchemaResult {
+  errors: Array<string>;
+  valid: boolean;
+}
+
+export class Schema {
+  protected compiled: Ajv.ValidateFunction;
+  protected compiler: Ajv.Ajv;
+
+  constructor(schema: any) {
+    this.compiler = new Ajv({
+      schemaId: 'auto',
+    });
+    this.compiled = this.compiler.compile(schema);
+  }
+
+  public match(value: any): SchemaResult {
+    const valid = this.compiled(value);
+    if (valid === true) {
+      return {
+        errors: [],
+        valid,
+      };
+    } else {
+      const errors = this.compiled.errors || [];
+      return {
+        errors: filterNil(errors.map((it) => it.message)),
+        valid: false,
+      };
+    }
+  }
+}
