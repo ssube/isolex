4a4e444f1bec386a035035a6a78a3fdbec0d646a
Sean Sube <seansube@gmail.com>
2018-12-16 17:40:20 -0600

    feat: relative includes for config (#19)

--- src/config/type/Include.ts
+++ src/config/type/Include.ts
@@ -1,17 +1,23 @@
-import { existsSync, readFileSync } from 'fs';
+import { existsSync, readFileSync, realpathSync } from 'fs';
 import { safeLoad, Type as YamlType } from 'js-yaml';
 import { BaseError } from 'noicejs';
+import { join } from 'path';
 
 import { CONFIG_SCHEMA } from 'src/config';
 
 export const includeType = new YamlType('!include', {
   kind: 'scalar',
   resolve(path: string) {
-    return existsSync(path);
+    const canonical = resolvePath(path);
+    if (existsSync(canonical)) {
+      return true;
+    } else {
+      throw new BaseError('included file does not exist');
+    }
   },
   construct(path: string): any {
     try {
-      return safeLoad(readFileSync(path, {
+      return safeLoad(readFileSync(resolvePath(path), {
         encoding: 'utf-8',
       }), {
         schema: CONFIG_SCHEMA,
@@ -21,3 +27,11 @@ export const includeType = new YamlType('!include', {
     }
   },
 });
+
+export function resolvePath(path: string): string {
+  if (path[0] === '.') {
+    return realpathSync(join(__dirname, path));
+  } else {
+    return realpathSync(path);
+  }
+}
