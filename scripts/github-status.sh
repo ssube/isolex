86fb6fa2b6628359e149a6ded2b5fe8c2f830b12
Sean Sube <seansube@gmail.com>
2018-12-17 14:13:28 -0600

    build: fix github status script

--- scripts/github-status.sh
+++ scripts/github-status.sh
@@ -1,13 +1,11 @@
-#! /bin/bash
+#! /bin/sh
 
-curl -d @- \
+CI_COMMIT_SHA="${CI_COMMIT_SHA:-$(git rev-parse HEAD)}"
+
+STATUS_BODY="{\"state\": \"success\", \"target_url\": \"${CI_PIPELINE_URL}\", \"description\": \"CI pipeline success!\", \"context\": \"continuous-integration/gitlab\"}"
+
+echo "Reporting status for ${CI_COMMIT_SHA}...\n${STATUS_BODY}"
+
+echo "${STATUS_BODY}" | curl -d @- \
   -H "Authorization: token $(echo -n "${GITHUB_SECRET}" | base64 -d)" \
-  "https://api.github.com/repos/ssube/isolex/statuses/${CI_COMMIT_SHA}" \
-  <<EOF
-{
-    "state": "success",
-    "target_url": "${CI_PIPELINE_URL}",
-    "description": "CI pipeline success!",
-    "context": "continuous-integration/gitlab"
-}
-EOF
+  -v "https://api.github.com/repos/ssube/isolex/statuses/${CI_COMMIT_SHA}" \
