accbeebc29d6ed48cf54d0152be71cf97a7a2386
Sean Sube <seansube@gmail.com>
2018-12-19 19:59:53 -0600

    lint: bash lint

--- scripts/github-status.sh
+++ scripts/github-status.sh
@@ -4,8 +4,8 @@ CI_COMMIT_SHA="${CI_COMMIT_SHA:-$(git rev-parse HEAD)}"
 
 STATUS_BODY="{\"state\": \"success\", \"target_url\": \"${CI_PIPELINE_URL}\", \"description\": \"CI pipeline success!\", \"context\": \"continuous-integration/gitlab\"}"
 
-echo "Reporting status for ${CI_COMMIT_SHA}...\n${STATUS_BODY}"
+printf "Reporting status for ${CI_COMMIT_SHA}...\n${STATUS_BODY}"
 
-echo "${STATUS_BODY}" | curl -d @- \
+printf "${STATUS_BODY}" | curl -d @- \
   -H "Authorization: token $(echo -n "${GITHUB_SECRET}" | base64 -d)" \
   -i "https://api.github.com/repos/ssube/isolex/statuses/${CI_COMMIT_SHA}"
