2bc565dcc9ad39d53b22582090f6264860a1f418
Sean Sube <seansube@gmail.com>
2018-12-29 17:54:10 -0600

    test: match and test schema regexp keyword (#92)

---
+++ test/schema/keyword/TestRegexp.ts
@@ -0,0 +1,46 @@
+import {expect} from 'chai';
+
+import { Schema } from 'src/schema';
+import { RuleValue } from 'src/utils/match';
+
+import { describeAsync, itAsync } from 'test/helpers/async';
+
+describeAsync('json schema', async () => {
+  describeAsync('regexp keyword', async () => {
+    itAsync('should match regexp instance', async () => {
+      const schema = new Schema();
+      const rule: RuleValue = {
+        regexp: /foo/,
+      };
+      const result = schema.match(rule, 'isolex#/definitions/match-rule-value');
+      expect(result.valid).to.equal(true);
+    });
+
+    itAsync('should match regexp flags', async () => {
+      const schema = new Schema({
+        properties: {
+          regexp: {
+            regexp: {
+              flags: 'gu',
+            },
+          },
+        },
+        type: 'object',
+      });
+      const rule: RuleValue = {
+        regexp: /foo/gu,
+      };
+      const result = schema.match(rule);
+      expect(result.valid).to.equal(true);
+    });
+
+    itAsync('should not match other things', async () => {
+      const schema = new Schema();
+      const rule = {
+        regexp: 'foo',
+      };
+      const result = schema.match(rule, 'isolex#/definitions/match-rule-value');
+      expect(result.valid).to.equal(false);
+    });
+  });
+});
