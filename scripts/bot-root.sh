deeb77c3a937992d93eadeac73b8412a7638def8
Sean Sube <seansube@gmail.com>
2019-01-02 10:08:50 -0600

    deps: patch typeorm and reflect-metadata

--- scripts/bot-root.sh
+++ scripts/bot-root.sh
@@ -3,5 +3,7 @@
 ROOT_ROLE_ID="$(uuidgen)"
 
 sqlite3 -echo out/isolex.db <<EOF
+DELETE FROM token;
+DELETE FROM user;
 INSERT INTO role ("id", "name", "grants") VALUES ('${ROOT_ROLE_ID}', 'root', '["*"]');
 EOF
