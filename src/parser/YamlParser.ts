e7a7d4a0c247aa0d30f0be3d0570b898085f03a8
Sean Sube <seansube@gmail.com>
2019-01-05 19:23:16 -0600

    lint: unused import

--- src/parser/YamlParser.ts
+++ src/parser/YamlParser.ts
@@ -10,7 +10,6 @@ import { Parser, ParserData, ParserOptions } from 'src/parser/Parser';
 import { mustExist } from 'src/utils';
 import { dictToMap } from 'src/utils/Map';
 import { TYPE_JSON, TYPE_YAML } from 'src/utils/Mime';
-import { TemplateScope } from 'src/utils/Template';
 
 export type YamlParserData = ParserData;
 export type YamlParserOptions = ParserOptions<YamlParserData>;
