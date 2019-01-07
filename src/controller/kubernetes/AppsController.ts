5e012d656fc3c155b55f68a5cf870543ec85004d
Sean Sube <seansube@gmail.com>
2019-01-07 08:42:03 -0600

    feat: list k8s daemon and stateful sets (#32)

--- src/controller/kubernetes/AppsController.ts
+++ src/controller/kubernetes/AppsController.ts
@@ -7,7 +7,9 @@ import { KubernetesBaseController, KubernetesBaseControllerData } from 'src/cont
 import { Command, CommandVerb } from 'src/entity/Command';
 import { mustExist } from 'src/utils';
 
+export const NOUN_DAEMONSET = 'kubernetes-daemonset';
 export const NOUN_DEPLOYMENT = 'kubernetes-deployment';
+export const NOUN_STATEFULSET = 'kubernetes-statefulset';
 
 export interface AppsControllerData extends KubernetesBaseControllerData {
   default: {
@@ -22,7 +24,11 @@ export class KubernetesAppsController extends KubernetesBaseController<AppsContr
   protected client?: k8s.Apps_v1Api;
 
   constructor(options: AppsControllerOptions) {
-    super(options, 'isolex#/definitions/service-controller-kubernetes-apps', [NOUN_DEPLOYMENT]);
+    super(options, 'isolex#/definitions/service-controller-kubernetes-apps', [
+      NOUN_DAEMONSET,
+      NOUN_DEPLOYMENT,
+      NOUN_STATEFULSET,
+    ]);
   }
 
   public async start() {
@@ -32,15 +38,36 @@ export class KubernetesAppsController extends KubernetesBaseController<AppsContr
     this.client = config.makeApiClient(k8s.Apps_v1Api);
   }
 
+  @Handler(NOUN_DAEMONSET, CommandVerb.List)
+  @CheckRBAC()
+  public async listDaemons(cmd: Command): Promise<void> {
+    const client = mustExist(this.client);
+    const ns = cmd.getHeadOrDefault('ns', this.data.default.namespace);
+    this.logger.debug({ cmd, ns }, 'listing k8s daemon sets');
+
+    const response = await client.listNamespacedDaemonSet(ns);
+    return this.transformJSON(cmd, response.body.items);
+  }
+
   @Handler(NOUN_DEPLOYMENT, CommandVerb.List)
   @CheckRBAC()
-  public async listDeployments(cmd: Command): Promise<void> {
+  public async listDeploys(cmd: Command): Promise<void> {
     const client = mustExist(this.client);
     const ns = cmd.getHeadOrDefault('ns', this.data.default.namespace);
     this.logger.debug({ cmd, ns }, 'listing k8s deployments');
 
     const response = await client.listNamespacedDeployment(ns);
-    this.logger.debug({ deployments: response.body }, 'found deployments');
+    return this.transformJSON(cmd, response.body.items);
+  }
+
+  @Handler(NOUN_STATEFULSET, CommandVerb.List)
+  @CheckRBAC()
+  public async listStatefuls(cmd: Command): Promise<void> {
+    const client = mustExist(this.client);
+    const ns = cmd.getHeadOrDefault('ns', this.data.default.namespace);
+    this.logger.debug({ cmd, ns }, 'listing k8s stateful sets');
+
+    const response = await client.listNamespacedStatefulSet(ns);
     return this.transformJSON(cmd, response.body.items);
   }
 }
