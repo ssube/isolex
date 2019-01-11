5cd16d306ae04d7b21d66eb4b85ec1532be5f845
Sean Sube <seansube@gmail.com>
2019-01-10 23:54:10 -0600

    lint: add types for patched k8s client

--- src/utils/kubernetes/AppsClient.ts
+++ src/utils/kubernetes/AppsClient.ts
@@ -1,18 +1,22 @@
 import * as k8s from '@kubernetes/client-node';
 
+export interface ScaleBody {
+  spec: k8s.V1ScaleSpec;
+}
+
 /**
  * This is a client with correct PATCH-request headers, working around the default header content types.
  * See/from https://github.com/kubernetes-client/javascript/issues/19#issuecomment-449424288
  */
 export class AppsClient extends k8s.Apps_v1Api {
-  public patchNamespacedDeploymentScale(name: string, ns: string, body: any) {
+  public patchNamespacedDeploymentScale(name: string, ns: string, body: ScaleBody) {
     const prev = this.patchHeaders();
     const req = super.patchNamespacedDeploymentScale(name, ns, body);
     this.defaultHeaders = prev;
     return req;
   }
 
-  public patchNamespacedStatefulSetScale(name: string, ns: string, body: any) {
+  public patchNamespacedStatefulSetScale(name: string, ns: string, body: ScaleBody) {
     const prev = this.patchHeaders();
     const req = super.patchNamespacedStatefulSetScale(name, ns, body);
     this.defaultHeaders = prev;
