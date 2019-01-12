eacb4b611c7d1b607b4f833aa2abfd6acb7a3b56
Sean Sube <seansube@gmail.com>
2019-01-12 16:19:25 -0600

    feat: bot ctrl can get metrics (#70)

--- src/controller/BotController.ts
+++ src/controller/BotController.ts
@@ -1,22 +1,49 @@
+import { Inject } from 'noicejs';
+import { Registry } from 'prom-client';
+
+import { INJECT_METRICS } from 'src/BaseService';
 import { CheckRBAC, Controller, ControllerData, Handler } from 'src/controller';
 import { BaseController, BaseControllerOptions } from 'src/controller/BaseController';
 import { Command, CommandVerb } from 'src/entity/Command';
 import { Context } from 'src/entity/Context';
+import { mustExist } from 'src/utils';
 import { VERSION_INFO } from 'src/version';
 
+export const NOUN_METRIC = 'bot-metric';
 export const NOUN_NOUN = 'bot-noun';
 export const NOUN_SERVICE = 'bot-service';
 export const NOUN_VERSION = 'bot-version';
 
 export type BotControllerData = ControllerData;
 
+@Inject(INJECT_METRICS)
 export class BotController extends BaseController<BotControllerData> implements Controller {
+  protected readonly metrics: Registry;
+
   constructor(options: BaseControllerOptions<BotControllerData>) {
     super(options, 'isolex#/definitions/service-controller-bot', [
+      NOUN_METRIC,
       NOUN_NOUN,
       NOUN_SERVICE,
       NOUN_VERSION,
     ]);
+
+    this.metrics = mustExist(options[INJECT_METRICS]);
+  }
+
+  @Handler(NOUN_METRIC, CommandVerb.Get)
+  @CheckRBAC()
+  public async getMetrics(cmd: Command, ctx: Context): Promise<void> {
+    const name = cmd.getHead('name');
+    const metrics = this.metrics.getSingleMetric(name);
+    return this.transformJSON(cmd, [metrics]);
+  }
+
+  @Handler(NOUN_METRIC, CommandVerb.List)
+  @CheckRBAC()
+  public async listMetrics(cmd: Command, ctx: Context): Promise<void> {
+    const metrics = this.metrics.getMetricsAsJSON();
+    return this.transformJSON(cmd, metrics);
   }
 
   @Handler(NOUN_NOUN, CommandVerb.List)
@@ -37,12 +64,14 @@ export class BotController extends BaseController<BotControllerData> implements 
   }
 
   @Handler(NOUN_SERVICE, CommandVerb.List)
+  @CheckRBAC()
   public async listServices(cmd: Command): Promise<void> {
     const services = Array.from(this.services.listServices());
     return this.transformJSON(cmd, services);
   }
 
   @Handler(NOUN_VERSION, CommandVerb.Get)
+  @CheckRBAC()
   public async getVersion(cmd: Command): Promise<void> {
     return this.transformJSON(cmd, VERSION_INFO);
   }
