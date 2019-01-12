ae7ed3e88cf879b8eeda64d6a974e7c6b145d0cb
Sean Sube <seansube@gmail.com>
2019-01-11 21:25:27 -0600

    feat: reaction ctrl matches many reactions, add to ref config

--- src/controller/ReactionController.ts
+++ src/controller/ReactionController.ts
@@ -1,20 +1,27 @@
 import { Inject } from 'noicejs';
 
-import { CheckRBAC, Controller, ControllerData, Handler } from 'src/controller';
+import { Controller, ControllerData, Handler } from 'src/controller';
 import { BaseController, BaseControllerOptions } from 'src/controller/BaseController';
 import { Command, CommandVerb } from 'src/entity/Command';
 import { Context } from 'src/entity/Context';
 import { Message } from 'src/entity/Message';
+import { Match, MatchData } from 'src/utils/match';
 import { TYPE_TEXT } from 'src/utils/Mime';
 
 export interface ReactionChance {
+  add: Array<string>;
   chance: number;
-  name: string;
+  match: MatchData;
 }
 
 export interface ReactionControllerData extends ControllerData {
-  field: string;
-  reactions: Map<string, Array<ReactionChance>>;
+  reactions: Array<ReactionChance>;
+}
+
+export interface CompiledReaction {
+  add: Array<string>;
+  chance: number;
+  match: Match;
 }
 
 export type ReactionControllerOptions = BaseControllerOptions<ReactionControllerData>;
@@ -23,30 +30,45 @@ export const NOUN_REACTION = 'reaction';
 
 @Inject()
 export class ReactionController extends BaseController<ReactionControllerData> implements Controller {
-  protected reactions: Map<string, Array<ReactionChance>>;
+  protected reactions: Array<CompiledReaction>;
 
   constructor(options: ReactionControllerOptions) {
     super(options, 'isolex#/definitions/service-controller-reaction', [NOUN_REACTION]);
 
-    this.reactions = new Map(Object.entries(options.data.reactions));
+    this.reactions = options.data.reactions.map((r) => {
+      return {
+        add: r.add,
+        chance: r.chance,
+        match: new Match(r.match),
+      };
+    });
   }
 
   @Handler(NOUN_REACTION, CommandVerb.Create)
-  @CheckRBAC()
   public async createReaction(cmd: Command, ctx: Context): Promise<void> {
     const reactions = [];
-    const body = cmd.get(this.data.field);
-    for (const [key, next] of this.reactions) {
-      this.logger.debug({ body, key }, 'checking reaction');
-      if (body.includes(key)) {
-        for (const reaction of next) {
-          const result = Math.random();
-          this.logger.debug({ body, key, next, result, reaction }, 'rolling reaction');
-          if (result < reaction.chance) {
-            reactions.push(reaction.name);
+    const bodies = cmd.get('body');
+    for (const body of bodies) {
+      for (const next of this.reactions) {
+        const results = next.match.match({
+          body,
+          cmd,
+        });
+        this.logger.debug({ body, next }, 'checking reaction');
+        if (results.matched) {
+          for (const reaction of next.add) {
+            const result = Math.random();
+            this.logger.debug({ next, result, reaction }, 'rolling reaction');
+            if (result < next.chance) {
+              reactions.push(reaction);
+            }
           }
         }
       }
+    }
+
+    if (reactions.length === 0) {
+      return;
     }
 
     this.logger.debug({ cmd, reactions }, 'reacting to command');
