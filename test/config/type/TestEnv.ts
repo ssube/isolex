80b4486892a53c11792aaf3f19191a4a58df058a
Sean Sube <seansube@gmail.com>
2019-01-13 22:27:03 -0600

    test: config loading, fix test file paths

--- test/config/type/TestEnv.ts
+++ test/config/type/TestEnv.ts
@@ -7,17 +7,17 @@ import { VERSION_INFO } from 'src/version';
 import { describeAsync, itAsync } from 'test/helpers/async';
 
 describeAsync('env config type', async () => {
-  itAsync('throw on missing variables', async () => {
+  itAsync('should throw on missing variables', async () => {
     expect(() => {
       envType.resolve('DOES_NOT_EXIST_');
     }).to.throw(NotFoundError);
   });
 
-  itAsync('resolve existing variables', async () => {
+  itAsync('should resolve existing variables', async () => {
     expect(envType.resolve('CI_COMMIT_SHA')).to.equal(true);
   });
 
-  itAsync('construct a value from variables', async () => {
+  itAsync('should construct a value from variables', async () => {
     expect(envType.construct('CI_COMMIT_SHA')).to.equal(VERSION_INFO.git.commit);
   });
 });
