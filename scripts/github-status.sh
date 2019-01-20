1234fcdd88f71f9d99bc2337d1133a237bcae8d3
Sean Sube <seansube@gmail.com>
2019-01-20 11:04:56 -0600

    build: use standard commit status

--- scripts/github-status.sh
+++ scripts/github-status.sh
@@ -3,7 +3,7 @@
 STATUS="${1}"
 CI_COMMIT_SHA="${CI_COMMIT_SHA:-$(git rev-parse HEAD)}"
 
-STATUS_BODY="{\"state\": \"${STATUS}\", \"target_url\": \"${CI_PIPELINE_URL}\", \"description\": \"CI pipeline ${STATUS}!\", \"context\": \"continuous-integration/gitlab\"}"
+STATUS_BODY="{\"state\": \"${STATUS}\", \"target_url\": \"${CI_PIPELINE_URL}\", \"description\": \"CI pipeline ${STATUS}!\", \"context\": \"gitlab/build\"}"
 
 printf "Reporting status for %s...\n%s" "${CI_COMMIT_SHA}" "${STATUS_BODY}"
 printf "%s" "${STATUS_BODY}" | curl -d @- \
