6211c12d58970e09412428a8105dc38c97fbc0b8
ssube <seansube@gmail.com>
2019-05-27 10:18:41 -0500

    feat(endpoint): add transforms to base endpoint, echo push hooks

--- src/endpoint/BaseEndpoint.ts
+++ src/endpoint/BaseEndpoint.ts
@@ -1,11 +1,33 @@
 import { Router } from 'express';
+import { Inject } from 'noicejs';
+import { Repository } from 'typeorm';
 
-import { BotService, BotServiceOptions } from 'src/BotService';
+import { BotService, BotServiceOptions, INJECT_STORAGE } from 'src/BotService';
 import { Endpoint, EndpointData } from 'src/endpoint';
+import { Context, ContextOptions } from 'src/entity/Context';
+import { BaseListenerOptions } from 'src/listener/BaseListener';
+import { mustExist } from 'src/utils';
+import { ServiceModule } from 'src/module/ServiceModule';
+import { INJECT_SERVICES } from 'src/BaseService';
+import { ServiceDefinition } from 'src/Service';
+import { TransformData, Transform } from 'src/transform';
 
 export type BaseEndpointOptions<TData extends EndpointData> = BotServiceOptions<TData>;
 
+@Inject(INJECT_STORAGE)
 export abstract class BaseEndpoint<TData extends EndpointData> extends BotService<TData> implements Endpoint {
+  protected readonly contextRepository: Repository<Context>;
+  protected readonly services: ServiceModule;
+  protected readonly transforms: Array<Transform>;
+
+  constructor(options: BaseListenerOptions<TData>, schemaPath: string) {
+    super(options, schemaPath);
+
+    this.contextRepository = mustExist(options[INJECT_STORAGE]).getRepository(Context);
+    this.services = mustExist(options[INJECT_SERVICES]);
+    this.transforms = [];
+  }
+
   public get paths(): Array<string> {
     return [
       this.id,
@@ -16,10 +38,21 @@ export abstract class BaseEndpoint<TData extends EndpointData> extends BotServic
   public abstract createRouter(): Promise<Router>;
 
   public async start(): Promise<void> {
-    /* noop */
+    await super.start();
+
+    const transforms: Array<ServiceDefinition<TransformData>> = this.data.transforms;
+    for (const def of transforms) {
+      const transform = await this.services.createService<Transform, TransformData>(def);
+      this.transforms.push(transform);
+    }
   }
 
-  public async stop(): Promise<void> {
-    /* noop */
+  protected async createContext(options: ContextOptions): Promise<Context> {
+    const ctx = await this.contextRepository.save(new Context({
+      ...options,
+      // TODO: does this need source/target?
+    }));
+    this.logger.debug({ ctx }, 'endpoint saved new context');
+    return ctx;
   }
 }
--- src/endpoint/GitlabEndpoint.ts
+++ src/endpoint/GitlabEndpoint.ts
@@ -3,6 +3,12 @@ import { json as expressJSON, Request, Response, Router } from 'express';
 import { BotServiceOptions } from 'src/BotService';
 import { Endpoint, EndpointData } from 'src/endpoint';
 import { BaseEndpoint } from 'src/endpoint/BaseEndpoint';
+import { Command, CommandVerb, CommandOptions } from 'src/entity/Command';
+import { applyTransforms } from 'src/transform/helpers';
+import { Message } from 'src/entity/Message';
+import { TYPE_JSON } from 'src/utils/Mime';
+import { mapToDict, dictValuesToArrays, Dict } from 'src/utils/Map';
+import { isString } from 'lodash';
 
 export interface GitlabBaseWebhook {
   object_kind: string;
@@ -77,8 +83,12 @@ export interface GitlabPushWebhook extends GitlabBaseWebhook {
   total_commits_count: number;
 }
 
-export class GitlabEndpoint extends BaseEndpoint<EndpointData> implements Endpoint {
-  constructor(options: BotServiceOptions<EndpointData>) {
+export interface GitlabEndpointData extends EndpointData {
+  defaultCommand: CommandOptions;
+}
+
+export class GitlabEndpoint extends BaseEndpoint<GitlabEndpointData> implements Endpoint {
+  constructor(options: BotServiceOptions<GitlabEndpointData>) {
     super(options, 'isolex#/definitions/service-endpoint-gitlab');
   }
 
@@ -101,57 +111,80 @@ export class GitlabEndpoint extends BaseEndpoint<EndpointData> implements Endpoi
       // authenticate & authorize
 
       const hook: GitlabBaseWebhook = req.body;
-      this.hookKind(req, res, hook);
+      return this.hookKind(req, res, hook);
     });
     return Promise.resolve(router);
   }
 
-  public hookKind(req: Request, res: Response, data: GitlabBaseWebhook) {
+  public async hookKind(req: Request, res: Response, data: GitlabBaseWebhook) {
     switch (data.object_kind) {
-        case 'issue':
-          this.issueHook(req, res, data as GitlabIssueWebhook);
-          return;
-        case 'job':
-          this.jobHook(req, res, data as GitlabJobWebhook);
-          return;
-        case 'note':
-          this.noteHook(req, res, data as GitlabNoteWebhook);
-          return;
-        case 'pipeline':
-          this.pipelineHook(req, res, data as GitlabPipelineWebhook);
-          return;
-        case 'push':
-          this.pushHook(req, res, data as GitlabPushWebhook);
-          return;
-        default:
-          this.logger.warn({
-            kind: data.object_kind,
-          }, 'unknown hook kind');
-      }
+      case 'issue':
+        return this.issueHook(req, res, data as GitlabIssueWebhook);
+      case 'job':
+        return this.jobHook(req, res, data as GitlabJobWebhook);
+      case 'note':
+        return this.noteHook(req, res, data as GitlabNoteWebhook);
+      case 'pipeline':
+        return this.pipelineHook(req, res, data as GitlabPipelineWebhook);
+      case 'push':
+        return this.pushHook(req, res, data as GitlabPushWebhook);
+      default:
+        this.logger.warn({
+          kind: data.object_kind,
+        }, 'unknown hook kind');
+    }
   }
 
-  public issueHook(req: Request, res: Response, data: GitlabIssueWebhook) {
+  public async issueHook(req: Request, res: Response, data: GitlabIssueWebhook) {
     this.logger.debug(data, 'gitlab issue hook');
     res.sendStatus(200);
   }
 
-  public jobHook(req: Request, res: Response, data: GitlabJobWebhook) {
+  public async jobHook(req: Request, res: Response, data: GitlabJobWebhook) {
     this.logger.debug(data, 'gitlab job hook');
     res.sendStatus(200);
   }
 
-  public noteHook(req: Request, res: Response, data: GitlabNoteWebhook) {
+  public async noteHook(req: Request, res: Response, data: GitlabNoteWebhook) {
     this.logger.debug(data, 'gitlab note hook');
     res.sendStatus(200);
   }
 
-  public pipelineHook(req: Request, res: Response, data: GitlabPipelineWebhook) {
+  public async pipelineHook(req: Request, res: Response, data: GitlabPipelineWebhook) {
     this.logger.debug(data, 'gitlab pipeline hook');
     res.sendStatus(200);
   }
 
-  public pushHook(req: Request, res: Response, data: GitlabPushWebhook) {
+  public async pushHook(req: Request, res: Response, data: GitlabPushWebhook) {
     this.logger.debug(data, 'gitlab push hook');
+    const context = await this.createContext({
+      channel: {
+        id: data.project.web_url,
+        thread: data.ref,
+      },
+      name: data.user_name,
+      uid: data.user_username,
+    });
+    // fake message for the transforms to check and filter
+    const msg = new Message({
+      context,
+      body: data.object_kind,
+      labels: this.labels,
+      reactions: [],
+      type: TYPE_JSON,
+    });
+    const txData = await applyTransforms(this.transforms, msg, TYPE_JSON, data);
+    if (Array.isArray(txData) || isString(txData)) {
+      this.logger.warn({data: txData}, 'transforms did not return object');
+    }
+    const cmd = new Command({
+      context,
+      data: dictValuesToArrays(txData as Dict<string>),
+      labels: this.labels,
+      noun: this.data.defaultCommand.noun,
+      verb: this.data.defaultCommand.verb,
+    });
+    await this.bot.executeCommand(cmd);
     res.sendStatus(200);
   }
 }
