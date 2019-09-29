30fe34f9aa7814e8fbd8543092f2236798287ae4
ssube <seansube@gmail.com>
2019-09-29 15:11:01 -0500

    fix(tests): test config validation mode flag

--- test/TestApp.ts
+++ test/TestApp.ts
@@ -6,14 +6,15 @@ import { spy } from 'sinon';
 import { createBot, CreateOptions, ExitStatus, main, runBot } from '../src/app';
 import { Bot, BotDefinition } from '../src/Bot';
 import { Service, ServiceEvent } from '../src/Service';
-import { defer } from '../src/utils/Async';
+import { defer, waitFor } from '../src/utils/Async';
 import { SIGNAL_RELOAD, SIGNAL_RESET, SIGNAL_STOP } from '../src/utils/Signal';
 import { describeLeaks, itLeaks } from './helpers/async';
 import { getTestLogger } from './helpers/logger';
 
-const MAIN_START_MULT = 10; // how much longer main takes vs normal start/signal tests
-const MAX_START_TIME = 500; // ms
-const MAX_SVC_TIME = 50;
+const MAIN_START_MULT = 20; // how much longer main takes vs normal start/signal tests
+const MAX_SIGNAL_TIME = 50; // ms
+const MAX_START_TIME = 250; // ms
+const MAX_SVC_TIME = 50; // ms
 
 const TEST_SERVICE = 'test-service';
 const TEST_CONFIG: BotDefinition = {
@@ -66,6 +67,41 @@ const TEST_CONFIG: BotDefinition = {
   },
 };
 
+const TEST_CONFIG_SERVICE: CreateOptions = {
+  config: {
+    data: {
+      ...TEST_CONFIG.data,
+      controllers: [{
+        data: {
+          filters: [],
+          strict: true,
+          transforms: [],
+        },
+        metadata: {
+          kind: TEST_SERVICE,
+          name: TEST_SERVICE,
+        },
+      }],
+    },
+    metadata: TEST_CONFIG.metadata,
+  },
+  logger: getTestLogger(),
+};
+
+const TEST_ARGS_VALID = [
+  '--config-name',
+  'isolex.yml',
+  '--config-path',
+  defaultTo(process.env.DOCS_PATH, __dirname),
+];
+
+const TEST_ARGS_INVALID = [
+  '--config-name',
+  'invalid.yml',
+  '--config-path',
+  defaultTo(process.env.DOCS_PATH, __dirname),
+];
+
 describeLeaks('app bot stuff', async () => {
   itLeaks('should create a bot and container', async () => {
     const { bot } = await createBot({
@@ -97,27 +133,7 @@ describeLeaks('app bot stuff', async () => {
 
   /* tslint:disable:no-identical-functions */
   itLeaks('should reset metrics while running', async () => {
-    const options: CreateOptions = {
-      config: {
-        data: {
-          ...TEST_CONFIG.data,
-          controllers: [{
-            data: {
-              filters: [],
-              strict: true,
-              transforms: [],
-            },
-            metadata: {
-              kind: TEST_SERVICE,
-              name: TEST_SERVICE,
-            },
-          }],
-        },
-        metadata: TEST_CONFIG.metadata,
-      },
-      logger: getTestLogger(),
-    };
-    const { bot, ctr } = await createBot(options);
+    const { bot, ctr } = await createBot(TEST_CONFIG_SERVICE);
 
     const notify = spy();
     const svc = ineeda<Service>({
@@ -125,10 +141,10 @@ describeLeaks('app bot stuff', async () => {
       stop: spy(),
     });
 
-    const [ module ] = ctr.getModules();
+    const [module] = ctr.getModules();
     module.bind(TEST_SERVICE).toInstance(svc);
 
-    const pendingStatus = runBot(options, bot);
+    const pendingStatus = runBot(TEST_CONFIG_SERVICE, bot);
     await Promise.race([
       pendingStatus,
       defer(MAX_START_TIME),
@@ -137,7 +153,7 @@ describeLeaks('app bot stuff', async () => {
     process.kill(process.pid, SIGNAL_RESET); // reset metrics
     await Promise.race([
       pendingStatus,
-      defer(MAX_START_TIME),
+      defer(MAX_SIGNAL_TIME),
     ]);
 
     process.kill(process.pid, SIGNAL_STOP); // ask it to stop
@@ -151,27 +167,7 @@ describeLeaks('app bot stuff', async () => {
   });
 
   itLeaks('should reload config while running', async () => {
-    const options: CreateOptions = {
-      config: {
-        data: {
-          ...TEST_CONFIG.data,
-          controllers: [{
-            data: {
-              filters: [],
-              strict: true,
-              transforms: [],
-            },
-            metadata: {
-              kind: TEST_SERVICE,
-              name: TEST_SERVICE,
-            },
-          }],
-        },
-        metadata: TEST_CONFIG.metadata,
-      },
-      logger: getTestLogger(),
-    };
-    const { bot, ctr } = await createBot(options);
+    const { bot, ctr } = await createBot(TEST_CONFIG_SERVICE);
 
     const notify = spy();
     const svc = ineeda<Service>({
@@ -179,10 +175,10 @@ describeLeaks('app bot stuff', async () => {
       stop: spy(),
     });
 
-    const [ module ] = ctr.getModules();
+    const [module] = ctr.getModules();
     module.bind(TEST_SERVICE).toInstance(svc);
 
-    const pendingStatus = runBot(options, bot);
+    const pendingStatus = runBot(TEST_CONFIG_SERVICE, bot);
     await Promise.race([
       pendingStatus,
       defer(MAX_START_TIME),
@@ -191,7 +187,7 @@ describeLeaks('app bot stuff', async () => {
     process.kill(process.pid, SIGNAL_RELOAD); // reset metrics
     await Promise.race([
       pendingStatus,
-      defer(MAX_START_TIME),
+      defer(MAX_SIGNAL_TIME),
     ]);
 
     process.kill(process.pid, SIGNAL_STOP); // ask it to stop
@@ -204,25 +200,73 @@ describeLeaks('app bot stuff', async () => {
       .and.been.calledWith(ServiceEvent.Stop);
 
   });
-});
 
-describeLeaks('main', async () => {
-  itLeaks('should return exit status', async () => {
-    const pendingStatus = main([
-      '--config-name',
-      'isolex.yml',
-      '--config-path',
-      defaultTo(process.env.DOCS_PATH, __dirname),
-    ]);
+  xit('should ignore unknown signals', async () => {
+    const { bot, ctr } = await createBot(TEST_CONFIG_SERVICE);
 
-    await Promise.race([
-      pendingStatus,
-      defer(MAX_START_TIME * MAIN_START_MULT),
-    ]);
+    const start = spy();
+    const notify = spy();
+    const svc = ineeda<Service>({
+      notify,
+      start,
+      stop: spy(),
+    });
+
+    const [module] = ctr.getModules();
+    module.bind(TEST_SERVICE).toInstance(svc);
+
+    const pendingStatus = runBot(TEST_CONFIG_SERVICE, bot);
+    await waitFor(() => {
+      return start.callCount > 0;
+    }, MAX_START_TIME, 1); // wait for the bot to start up
+
+    process.kill(process.pid, 1); // @TODO: figure out what signal to use
+    await defer(MAX_SIGNAL_TIME);
 
     process.kill(process.pid, SIGNAL_STOP); // ask it to stop
     const status = await pendingStatus;
 
     expect(status).to.equal(ExitStatus.Success);
+    expect(notify).to.have.callCount(2)
+      .and.been.calledWith(ServiceEvent.Start)
+      .and.been.calledWith(ServiceEvent.Stop);
+  });
+});
+
+/* tslint:disable:no-unbound-method */
+describeLeaks('main', async () => {
+  itLeaks('should return exit status', async () => {
+    const pendingStatus = main(TEST_ARGS_VALID);
+    await defer(MAX_START_TIME * MAIN_START_MULT);
+
+    process.kill(process.pid, SIGNAL_STOP); // ask it to stop
+    const timeout = defer(MAX_SIGNAL_TIME * MAIN_START_MULT);
+    const status = await Promise.race([pendingStatus, timeout]);
+
+    expect(status).to.equal(ExitStatus.Success);
+  });
+
+  itLeaks('should validate the config before starting', async () => {
+    const status = await main(TEST_ARGS_INVALID);
+
+    expect(status).to.equal(ExitStatus.Error);
+  });
+
+  itLeaks('should test config without starting', async () => {
+    const status = await main([
+      ...TEST_ARGS_VALID,
+      '--test',
+    ]);
+
+    expect(status).to.equal(ExitStatus.Success);
+  });
+
+  itLeaks('should fail when testing invalid config', async () => {
+    const status = await main([
+      ...TEST_ARGS_INVALID,
+      '--test',
+    ]);
+
+    expect(status).to.equal(ExitStatus.Error);
   });
 });
