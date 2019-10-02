9347054695e0a68772eff6a6e4cfa81938283b67
ssube <seansube@gmail.com>
2019-10-01 22:23:46 -0500

    fix(tests): cover template compiler

---
+++ test/utils/TestTemplateCompiler.ts
@@ -0,0 +1,56 @@
+import { expect } from 'chai';
+
+import { Context } from '../../src/entity/Context';
+import { TemplateCompiler } from '../../src/utils/TemplateCompiler';
+import { describeLeaks, itLeaks } from '../helpers/async';
+import { createContainer } from '../helpers/container';
+
+describeLeaks('template compiler', async () => {
+  describeLeaks('format context helper', async () => {
+    itLeaks('should prefix a name', async () => {
+      const { container } = await createContainer();
+      const compiler = await container.create(TemplateCompiler);
+      const ctx = new Context({
+        channel: {
+          id: '',
+          thread: '',
+        },
+        name: 'foo',
+        uid: '',
+      });
+      expect(compiler.formatContext(ctx)).to.equal('@foo');
+    });
+  });
+
+  describeLeaks('format entries helper', async () => {
+    itLeaks('should format a map', async () => {
+      const { container } = await createContainer();
+      const compiler = await container.create(TemplateCompiler);
+      const data = new Map([
+        ['foo', ''],
+        ['bar', ''],
+      ]);
+      expect(compiler.formatEntries(data, {
+        fn: () => 'foo', // block fn produces the items in the final join
+        hash: '',
+        inverse: () => '',
+      })).to.equal('foofoo');
+    });
+  });
+
+  describeLeaks('format json helper', async () => {
+    itLeaks('should flatten objects', async () => {
+      const { container } = await createContainer();
+      const compiler = await container.create(TemplateCompiler);
+      expect(compiler.formatJSON({}).toString()).to.equal('{}');
+    });
+  });
+
+  describeLeaks('format trim helper', async () => {
+    itLeaks('should trim strings to length', async () => {
+      const { container } = await createContainer();
+      const compiler = await container.create(TemplateCompiler);
+      expect(compiler.formatTrim('hello world', 6, '!')).to.equal('hello!');
+    });
+  });
+});
