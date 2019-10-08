6bae555894b5ee116088e18335b83664d1fda8da
ssube <seansube@gmail.com>
2019-10-08 18:35:11 -0500

    lint: rename method type to differentiate from decorator

--- src/endpoint/index.ts
+++ src/endpoint/index.ts
@@ -1,5 +1,4 @@
 import { Request, Response, Router } from 'express';
-import { BaseError } from 'noicejs';
 import passport from 'passport';
 
 import { BotServiceData } from '../BotService';
@@ -19,32 +18,13 @@ export interface Endpoint extends Service {
   createRouter(options: RouterOptions): Promise<Router>;
 }
 
-export enum HttpVerb {
-  Delete = 'DELETE',
-  Get = 'GET',
-  Head = 'HEAD',
-  Options = 'OPTIONS',
-  Post = 'POST',
-  Put = 'PUT',
-}
-
-export function commandVerbFor(verb: HttpVerb): CommandVerb {
-  switch (verb) {
-    case HttpVerb.Delete: return CommandVerb.Delete;
-    case HttpVerb.Get: return CommandVerb.Get;
-    case HttpVerb.Head: return CommandVerb.List;
-    case HttpVerb.Options: return CommandVerb.Help;
-    case HttpVerb.Post: return CommandVerb.Create;
-    case HttpVerb.Put: return CommandVerb.Update;
-    default: throw new BaseError('unknown verb');
-  }
-}
-
 export interface HandlerMetadata {
   grants: Array<string>;
   path: string;
   verb: CommandVerb;
 }
+
+export type HandlerMethod = (req: Request, res: Response) => Promise<void>;
 
 const HANDLER_KEY = Symbol('handler-metadata');
 export function Handler(verb: CommandVerb, path: string, grants: Array<string> = []) {
@@ -60,8 +40,6 @@ export function Handler(verb: CommandVerb, path: string, grants: Array<string> =
   };
 }
 
-export function getHandlerMetadata(target: Handler): HandlerMetadata {
+export function getHandlerMetadata(target: HandlerMethod): HandlerMetadata {
   return Reflect.get(target, HANDLER_KEY);
 }
-
-export type Handler = (req: Request, res: Response) => Promise<void>;
