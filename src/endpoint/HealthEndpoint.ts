2dcfc173908e7030d2d814e6a7bcb898eca66390
ssube <seansube@gmail.com>
2019-10-08 17:31:11 -0500

    bump main test timeouts

--- src/endpoint/HealthEndpoint.ts
+++ src/endpoint/HealthEndpoint.ts
@@ -26,9 +26,9 @@ export class HealthEndpoint extends BaseEndpoint<EndpointData> implements Endpoi
   public async getLiveness(req: Request, res: Response): Promise<void> {
     this.logger.debug('health endpoint get liveness');
     if (this.bot.isConnected) {
-      res.status(STATUS_SUCCESS).send(BODY_SUCCESS);
+      res.sendStatus(STATUS_SUCCESS);
     } else {
-      res.status(STATUS_ERROR).send(BODY_ERROR);
+      res.sendStatus(STATUS_ERROR);
     }
   }
 
@@ -36,9 +36,9 @@ export class HealthEndpoint extends BaseEndpoint<EndpointData> implements Endpoi
   public async getReadiness(req: Request, res: Response): Promise<void> {
     this.logger.debug('health endpoint get readiness');
     if (this.bot.getStorage().isConnected) {
-      res.status(STATUS_SUCCESS).send(BODY_SUCCESS);
+      res.sendStatus(STATUS_SUCCESS);
     } else {
-      res.status(STATUS_ERROR).send(BODY_ERROR);
+      res.sendStatus(STATUS_ERROR);
     }
   }
 }
