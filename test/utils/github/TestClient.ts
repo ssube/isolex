6b7e1ba65c950dbd2328cccf700791489bc1ed37
ssube <seansube@gmail.com>
2019-10-12 16:39:02 -0500

    fix(tests): cover github token renewal

--- test/utils/github/TestClient.ts
+++ test/utils/github/TestClient.ts
@@ -2,11 +2,23 @@ import { App } from '@octokit/app';
 import Octokit from '@octokit/rest';
 import { expect } from 'chai';
 import { ineeda } from 'ineeda';
-import { match, stub } from 'sinon';
+import { match, spy, stub } from 'sinon';
 
+import { defer } from '../../../src/utils/Async';
 import { GithubClient } from '../../../src/utils/github';
 import { describeLeaks, itLeaks } from '../../helpers/async';
 import { createServiceContainer } from '../../helpers/container';
+
+const TEST_DATA = {
+  agent: '',
+  app: {
+    id: 0,
+    key: 'invalued',
+  },
+  installation: {
+    id: 0,
+  },
+};
 
 // tslint:disable:no-any
 describeLeaks('github client', async () => {
@@ -18,16 +30,7 @@ describeLeaks('github client', async () => {
     const { container } = await createServiceContainer();
     const client = await container.create(GithubClient, {
       app,
-      data: {
-        agent: '',
-        app: {
-          id: 0,
-          key: '',
-        },
-        installation: {
-          id: 0,
-        },
-      },
+      data: TEST_DATA,
     });
     const token = await client.renewToken();
 
@@ -36,21 +39,22 @@ describeLeaks('github client', async () => {
   });
 
   itLeaks('should poll for new tokens', async () => {
-    const kit = stub().returns(ineeda<Octokit>()) as any;
+    const getInstallationAccessToken = stub().returns('test');
+    const app = stub().returns(ineeda<App>({
+      getInstallationAccessToken,
+    })) as any;
+    const kit = stub().yieldsToAsync('auth', []).returns(ineeda<Octokit>()) as any;
     const { container } = await createServiceContainer();
-    await container.create(GithubClient, {
-      data: {
-        agent: '',
-        app: {
-          id: 0,
-          key: '',
-        },
-        installation: {
-          id: 0,
-        },
-      },
+    const client = await container.create(GithubClient, {
+      app,
+      data: TEST_DATA,
       kit,
     });
+    const renewSpy = spy(client, 'renewToken');
+    await defer(0);
+
+    expect(getInstallationAccessToken).to.have.callCount(1);
     expect(kit).to.have.been.calledWithMatch(match.has('auth', match.func));
+    expect(renewSpy).to.have.callCount(1);
   });
 });
