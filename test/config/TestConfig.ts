f00d08c1572a9cd2debc3d0868e6852c63e847a7
ssube <seansube@gmail.com>
2019-10-16 18:21:40 -0500

    lint(tests): remove redundant suite names

--- test/config/TestConfig.ts
+++ test/config/TestConfig.ts
@@ -5,15 +5,13 @@ import { loadConfig } from '../../src/config';
 import { NotFoundError } from '../../src/error/NotFoundError';
 import { describeLeaks, itLeaks } from '../helpers/async';
 
-describeLeaks('config helpers', async () => {
-  describeLeaks('load config', async () => {
-    itLeaks('should load an existing config', async () => {
-      const config = await loadConfig('isolex.yml', join(__dirname, '..', 'docs'));
-      expect(config.metadata.kind).to.equal('bot');
-    });
+describeLeaks('load config helper', async () => {
+  itLeaks('should load an existing config', async () => {
+    const config = await loadConfig('isolex.yml', join(__dirname, '..', 'docs'));
+    expect(config.metadata.kind).to.equal('bot');
+  });
 
-    itLeaks('should throw when config is missing', async () => {
-      return expect(loadConfig('missing.yml', join(__dirname, '..', 'docs'))).to.eventually.be.rejectedWith(NotFoundError);
-    });
+  itLeaks('should throw when config is missing', async () => {
+    return expect(loadConfig('missing.yml', join(__dirname, '..', 'docs'))).to.eventually.be.rejectedWith(NotFoundError);
   });
 });
--- test/module/TestLoad.ts
+++ test/module/TestLoad.ts
@@ -5,44 +5,42 @@ import { loadModules } from '../../src/module';
 import { describeLeaks, itLeaks } from '../helpers/async';
 import { getTestLogger } from '../helpers/logger';
 
-describeLeaks('module helpers', async () => {
-  describeLeaks('main module helper', async () => {
-    itLeaks('should instatiate preset modules');
-    itLeaks('should append passed modules');
+describeLeaks('main module helper', async () => {
+  itLeaks('should instatiate preset modules');
+  itLeaks('should append passed modules');
+});
+
+describeLeaks('load plugin module helper', async () => {
+  xit('should load a plugin module', async () => {
+    const data = {
+      modules: [{
+        export: 'example-module',
+        require: 'isolex-oot-example',
+      }],
+    };
+    const modules = await loadModules({
+      data: data as BotData,
+      metadata: {
+        kind: '',
+        name: '',
+      },
+    }, getTestLogger(true));
+    expect(modules.length).to.equal(1);
   });
 
-  describeLeaks('load plugin module helper', async () => {
-    xit('should load a plugin module', async () => {
-      const data = {
-        modules: [{
-          export: 'example-module',
-          require: 'isolex-oot-example',
-        }],
-      };
-      const modules = await loadModules({
-        data: data as BotData,
-        metadata: {
-          kind: '',
-          name: '',
-        },
-      }, getTestLogger(true));
-      expect(modules.length).to.equal(1);
-    });
-
-    itLeaks('should handle errors while loading plugin modules', async () => {
-      const data = {
-        modules: [{
-          export: 'default',
-          require: 'isolex-oot-missing',
-        }],
-      };
-      return expect(loadModules({
-        data: data as BotData,
-        metadata: {
-          kind: '',
-          name: '',
-        },
-      }, getTestLogger())).to.eventually.deep.equal([]);
-    });
+  itLeaks('should handle errors while loading plugin modules', async () => {
+    const data = {
+      modules: [{
+        export: 'default',
+        require: 'isolex-oot-missing',
+      }],
+    };
+    return expect(loadModules({
+      data: data as BotData,
+      metadata: {
+        kind: '',
+        name: '',
+      },
+    }, getTestLogger())).to.eventually.deep.equal([]);
   });
 });
