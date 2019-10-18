f57075399caca11efdc558bcf8b5b5e41d4fb64c
ssube <seansube@gmail.com>
2019-10-18 16:52:04 -0500

    fix(tests): cover trim string helper

--- test/utils/TestString.ts
+++ test/utils/TestString.ts
@@ -1,9 +1,12 @@
 import { expect } from 'chai';
 
-import { leftPad } from '../../src/utils/String';
+import { leftPad, trim } from '../../src/utils/String';
 import { describeLeaks, itLeaks } from '../helpers/async';
 
-describeLeaks('left pad', async () => {
+const TEST_SHORT = 'hello';
+const TEST_LONG = 'hello world';
+
+describeLeaks('left pad helper', async () => {
   itLeaks('should prepend padding', async () => {
     expect(leftPad('test')).to.equal('0000test');
   });
@@ -15,5 +18,24 @@ describeLeaks('left pad', async () => {
 
   itLeaks('should use padding string', async () => {
     expect(leftPad('test', 8, 'too')).to.equal('toottest', 'must repeat and truncate the padding string');
+  });
+});
+
+describeLeaks('trim helper', async () => {
+  itLeaks('should return strings shorter than max', async () => {
+    expect(trim('yes', 5)).to.equal('yes', 'shorter than max');
+    expect(trim(TEST_SHORT, 5)).to.equal(TEST_SHORT, 'equal to max');
+  });
+
+  itLeaks('should trim strings longer than max', async () => {
+    expect(trim(TEST_LONG, 3, '...')).to.equal('...');
+    expect(trim(TEST_LONG, 5)).to.equal('he...');
+    expect(trim(TEST_LONG, 8)).to.equal('hello...');
+  });
+
+  itLeaks('should not add tail when max is small', async () => {
+    expect(trim(TEST_SHORT, 2, '...')).to.equal('he');
+    expect(trim(TEST_LONG, 5, 'very long tail')).to.equal(TEST_SHORT);
+    expect(trim(TEST_SHORT, 8, 'very long tail')).to.equal(TEST_SHORT);
   });
 });
