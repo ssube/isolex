e3979d6cfccaf70e9f59fef53991f018a638e00b
ssube <seansube@gmail.com>
2019-10-20 22:24:14 -0500

    feat(endpoint/github): execute pull review and status hooks

--- src/endpoint/GithubEndpoint.ts
+++ src/endpoint/GithubEndpoint.ts
@@ -5,11 +5,13 @@ import { Counter } from 'prom-client';
 import { Handler } from '.';
 import { INJECT_METRICS } from '../BaseService';
 import { INJECT_STORAGE } from '../BotService';
-import { CommandVerb } from '../entity/Command';
+import { Command, CommandVerb } from '../entity/Command';
+import { Context } from '../entity/Context';
 import { mustExist } from '../utils';
 import { createServiceCounter, incrementServiceCounter } from '../utils/Metrics';
-import { BaseEndpoint, BaseEndpointOptions, STATUS_SUCCESS } from './BaseEndpoint';
-import { HookEndpointData } from './HookEndpoint';
+import { TemplateScope } from '../utils/Template';
+import { BaseEndpointOptions, STATUS_SUCCESS } from './BaseEndpoint';
+import { HookEndpoint, HookEndpointData } from './HookEndpoint';
 
 export interface GithubEndpointData extends HookEndpointData {
   secret: string;
@@ -17,8 +19,9 @@ export interface GithubEndpointData extends HookEndpointData {
 
 export type GithubEndpointOptions = BaseEndpointOptions<GithubEndpointData>;
 
+// tslint:disable:no-any
 @Inject(INJECT_METRICS, INJECT_STORAGE)
-export class GithubEndpoint extends BaseEndpoint<GithubEndpointData> {
+export class GithubEndpoint extends HookEndpoint<GithubEndpointData> {
   protected readonly hookCounter: Counter;
 
   constructor(options: BaseEndpointOptions<GithubEndpointData>) {
@@ -43,6 +46,7 @@ export class GithubEndpoint extends BaseEndpoint<GithubEndpointData> {
     const eventType = req.header('X-GitHub-Event');
     const eventId = req.header('X-GitHub-Delivery');
     const eventData = req.body;
+
     this.logger.info({
       eventData,
       eventId,
@@ -51,6 +55,64 @@ export class GithubEndpoint extends BaseEndpoint<GithubEndpointData> {
     incrementServiceCounter(this, this.hookCounter, {
       eventType,
     });
+
+    switch (eventType) {
+      case 'pull_request_review':
+        return this.pullReviewHook(req, res, eventData);
+      case 'status':
+        return this.statusHook(req, res, eventData);
+      default:
+        this.logger.warn({
+          kind: eventType,
+        }, 'unknown hook kind');
+        res.sendStatus(STATUS_SUCCESS);
+    }
+  }
+
+  public async pullReviewHook(req: Request, res: Response, data: any): Promise<void> {
+    this.logger.debug(data, 'github pull review hook');
+    const user = mustExist(this.hookUser);
+    const ctx = await this.createContext({
+      channel: {
+        id: data.repository.full_name,
+        thread: data.pull_request.number,
+      },
+      name: data.sender.login,
+      uid: this.data.hookUser,
+      user,
+    });
+    const cmd = await this.createHookCommand(ctx, data, 'pull_request_review');
+    await this.bot.executeCommand(cmd);
     res.sendStatus(STATUS_SUCCESS);
+  }
+
+  public async statusHook(req: Request, res: Response, data: any): Promise<void> {
+    this.logger.debug(data, 'github commit status hook');
+    const user = mustExist(this.hookUser);
+    const ctx = await this.createContext({
+      channel: {
+        id: data.repository.full_name,
+        thread: data.sha,
+      },
+      name: data.sender.login,
+      uid: this.data.hookUser,
+      user,
+    });
+    const cmd = await this.createHookCommand(ctx, data, 'status');
+    await this.bot.executeCommand(cmd);
+    res.sendStatus(STATUS_SUCCESS);
+  }
+
+  protected async createHookCommand(context: Context, data: TemplateScope, kind: string): Promise<Command> {
+    const labels = new Map(this.labels);
+    labels.set('hook', kind);
+
+    return new Command({
+      context,
+      data,
+      labels,
+      noun: this.data.defaultCommand.noun,
+      verb: this.data.defaultCommand.verb,
+    });
   }
 }
