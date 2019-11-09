46a998b557ffb5bd323bd86614ff4e2161f9e995
ssube <seansube@gmail.com>
2019-11-09 11:30:34 -0600

    feat(build): add target to globally link bin

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -3,7 +3,7 @@
 IMAGE_PUSH="${1:---skip}"
 
 IMAGE_NAME="${CI_PROJECT_PATH}"
-IMAGE_TAG="$(echo "${CI_COMMIT_TAG:-${CI_COMMIT_REF_SLUG}}" | sed -r 's/[^-_a-zA-Z0-9]/-/g')"
+IMAGE_TAG="$(echo "${CI_COMMIT_TAG:-${CI_COMMIT_REF_SLUG}}" | sed -r 's/[^-_a-zA-Z0-9\\.]/-/g')"
 IMAGE_FULL="${IMAGE_NAME}:${IMAGE_TAG}"
 
 echo "Building image: ${IMAGE_FULL}"
