21020aa6969a41cbc165c5b90813694ec7904cd3
ssube <seansube@gmail.com>
2019-12-29 10:09:22 -0600

    fix(scripts): filter PR approval by author

--- scripts/filter/github-status.sh
+++ scripts/filter/github-status.sh
@@ -2,6 +2,7 @@
 
 # GITHUB_COMMIT
 # GITHUB_PROJECT
+# GITHUB_PR_ID
 # GITHUB_TOKEN
 
 STATUS_REQUIRED="codecov/patch
@@ -9,11 +10,23 @@ gitlab/build"
 
 URL_BASE="https://api.github.com/repos/${GITHUB_PROJECT}"
 
+URL_PULL_REQUEST="${URL_BASE}/pulls/${GITHUB_PR_ID}"
 URL_COMMIT_STATUS="${URL_BASE}/commits/${GITHUB_COMMIT}/statuses"
 
+PULL_REQUEST_DATA="$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" "${URL_PULL_REQUEST}")"
+PULL_REQUEST_AUTHOR="$(echo "${PULL_REQUEST_DATA}" | jq -r '.user.login')"
+
+echo "Author: ${PULL_REQUEST_AUTHOR}"
+
+if [[ "${PULL_REQUEST_AUTHOR}" =~ ^(ssube|renovate.*)$ ]];
+then
+  echo "Trusted PR author."
+else
+  echo "Unknown PR author."
+  exit 1
+fi
+
 COMMIT_STATUS_DATA="$(curl -s -H "Authorization: token ${GITHUB_TOKEN}" "${URL_COMMIT_STATUS}")"
-# COMMIT_STATUS_FAILURE="$(echo "${COMMIT_STATUS_DATA}" | jq -r '.[] | select(.state == "failure") | .context' | sort | uniq)"
-# COMMIT_STATUS_PENDING="$(echo "${COMMIT_STATUS_DATA}" | jq -r '.[] | select(.state == "pending") | .context' | sort | uniq)"
 COMMIT_STATUS_SUCCESS="$(echo "${COMMIT_STATUS_DATA}" | jq -r '.[] | select(.state == "success") | .context' | sort | uniq)"
 
 STATUS_PASSED=""
