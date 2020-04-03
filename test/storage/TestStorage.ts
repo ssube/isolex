d1b094740542cdc118df5e3a2f4aa7cf339755cf
ssube <seansube@gmail.com>
2020-04-02 22:55:34 -0500

    fix(test): cover storage service

---
+++ test/storage/TestStorage.ts
@@ -0,0 +1,51 @@
+import { NotFoundError } from '@apextoaster/js-utils';
+import { expect } from 'chai';
+
+import { EntityModule } from '../../src/module/EntityModule';
+import { MigrationModule } from '../../src/module/MigrationModule';
+import { Storage } from '../../src/storage';
+import { createServiceContainer } from '../helpers/container';
+
+const TEST_METADATA = {
+  kind: 'test-storage',
+  name: 'test-storage',
+};
+
+describe('storage service', async () => {
+  xit('should handle connection errors');
+
+  it('should close connections when stopped', async () => {
+    const { container } = await createServiceContainer(new EntityModule(), new MigrationModule());
+    const storage = await container.create(Storage, {
+      data: {
+        migrate: false,
+        orm: {
+          database: 'out/test.db',
+          type: 'sqlite',
+        },
+      },
+      metadata: TEST_METADATA,
+    });
+    await storage.start();
+    await storage.stop();
+    expect(storage.isConnected).to.equal(false);
+  });
+
+  it('should check connection status', async () => {
+    const { container } = await createServiceContainer(new EntityModule(), new MigrationModule());
+    const storage = await container.create(Storage, {
+      data: {
+        migrate: false,
+        orm: {
+          database: 'test.db',
+          type: 'sqlite',
+        },
+      },
+      metadata: TEST_METADATA,
+    });
+    await storage.start();
+    const connected = storage.isConnected;
+    await storage.stop();
+    expect(connected).to.equal(true);
+  });
+});
