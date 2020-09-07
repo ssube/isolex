b5a72df611cef80e34a1e4205d9345e41b2ad754
ssube <seansube@gmail.com>
2020-09-07 10:05:26 -0500

    externalize prod deps

--- config/rollup/project.js
+++ config/rollup/project.js
@@ -18,5 +18,32 @@ module.exports = {
 				'Issuer.useGot()': '/* do not use got */',
 			},
 		}),
+		replace({
+			delimiters: ['', ''],
+			values: {
+				/* unnecessary drivers loaded by TypeORM */
+				"require('@sap/hdbext')": 'undefined',
+				"require('ioredis')": 'undefined',
+				"require('mongodb')": 'undefined',
+				"require('mssql')": 'undefined',
+				"require('mysql')": 'undefined',
+				"require('mysql2')": 'undefined',
+				"require('oracledb')": 'undefined',
+				"require('pg')": 'undefined',
+				"require('pg-native')": 'undefined',
+				"require('pg-query-stream')": 'undefined',
+				"require('react-native-sqlite-storage')": 'undefined',
+				"require('redis')": 'undefined',
+				"require('sql.js')": 'undefined',
+				"require('typeorm-aurora-data-api-driver')": 'undefined',
+				/* other packages that need to be removed */
+				"require('@discordjs/uws')": 'undefined',
+				"require('electron')": 'undefined',
+				"require('erlpack')": 'undefined',
+				"require('ffmpeg-binaries')": 'undefined',
+				"require('node-opus')": 'undefined',
+				"require('opusscript')": 'undefined',
+			},
+		}),
 	],
 };
